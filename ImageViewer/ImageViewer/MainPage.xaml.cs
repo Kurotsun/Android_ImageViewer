893f0e65df9c7f6b662b80b445661b776a064c9c
Kurotsun <97564308+Kurotsun@users.noreply.github.com>
2022-01-22 13:56:42 -0500

    Add project files.

---
+++ ImageViewer/ImageViewer/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageViewer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void UpdateImage(object sender, EventArgs e)
+        {
+            Image.Source = new UriImageSource
+            {
+                Uri = new Uri(ImageUrl.Text),
+                CachingEnabled = true,
+                CacheValidity = new TimeSpan(5, 0, 0, 0)
+            };
+
+        }
+    }
+}
